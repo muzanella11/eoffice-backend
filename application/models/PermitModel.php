d21a35809d1dcfb72a865aa8516fc6d0a80becd4
Nurfirliana Muzanella <muzanella11@gmail.com>
2019-03-28 23:07:33 +0800

    [FEATURE] Permit management (#5)

---
+++ application/models/PermitModel.php
@@ -0,0 +1,164 @@
+<?php
+
+/**
+ * @author f1108k
+ * @copyright 2018
+ */
+
+
+
+?>
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+    class PermitModel extends MY_Model {
+        private $tableName = 'permit';
+
+        function __construct(){
+            parent::__construct();
+        }
+
+        function addDataPermit ($data) {
+            $sql    =   "INSERT INTO {$this->tableName} (
+				name,
+				reason,
+				start_date,
+				end_date,
+				created_at
+			) VALUES (
+				'".$data['name']."',
+				'".$data['reason']."',
+				'".$data['start_date']."',
+				'".$data['end_date']."',
+				now()
+			)";
+            
+            $query  =   $this->db->query($sql);
+            $latestId = $this->db->insert_id();
+
+            if ($this->db->trans_status() === FALSE)
+            {
+                $this->db->trans_rollback();
+            }
+            else
+            {
+                $this->db->trans_commit();
+            }
+
+            $getError = $this->db->error();
+
+            if (!$getError['message']) {
+                return [
+                    'latest_create_id' => $latestId,
+                    'flag' => 0,
+                    'messages' => 'Berhasil menambahkan data'
+                ];
+            } else {
+                return [
+                    'flag' => 1,
+                    'messages' => 'Gagal menambahkan data'
+                ];
+            }
+        }
+
+        function getDataPermit ($filter = NULL, $filter_key = NULL, $limit = NULL, $field_target = NULL) {
+            if(!empty($filter) && !empty($filter_key)) {
+                if($filter === 'id') {
+                    if(is_array($limit)) {
+                        $sql    =   "SELECT * FROM {$this->tableName} WHERE id='".$filter_key."' LIMIT ".$limit['startLimit'].",".$limit['limitData']."";
+                    } else {
+                        $sql    =   "SELECT * FROM {$this->tableName} WHERE id='".$filter_key."'";
+                    }
+                } elseif ($filter === 'search') {
+                    if(is_array($limit)) {
+                        $sql    =   "SELECT * FROM {$this->tableName} WHERE ".$field_target." LIKE '%".$filter_key."%' LIMIT ".$limit['startLimit'].",".$limit['limitData']."";
+                    } else {
+                        $sql    =   "SELECT * FROM {$this->tableName} WHERE ".$field_target." LIKE '%".$filter_key."%'";
+                    }
+                } elseif ($filter === 'name') {
+                    if(is_array($limit)) {
+                        $sql    =   "SELECT * FROM {$this->tableName} WHERE name='".$filter_key."' LIMIT ".$limit['startLimit'].",".$limit['limitData']."";
+                    } else {
+                        $sql    =   "SELECT * FROM {$this->tableName} WHERE name='".$filter_key."'";
+                    }
+                } elseif ($filter === 'create_sql') {
+                    if(is_array($limit)) {
+                        $sql    =   "SELECT * FROM {$this->tableName} ".$filter_key." LIMIT ".$limit['startLimit'].",".$limit['limitData']."";
+                    } else {
+                        $sql    =   "SELECT * FROM {$this->tableName} ".$filter_key."";
+                    }
+                }
+            } else {
+                if(is_array($limit)) {
+                    $sql    =   "SELECT * FROM {$this->tableName} LIMIT ".$limit['startLimit'].",".$limit['limitData']."";
+                } else {
+                    $sql    =   "SELECT * FROM {$this->tableName}";
+                }
+			}
+
+            $query  =   $this->db->query($sql);
+            $getError = $this->db->error();
+
+            if (!$getError['message'] && $query->num_rows() > 0) {
+                return $query->result();
+            } else {
+                return [];
+            }
+        }
+
+        function updateDataPermit ($data, $findByValue = '') {
+			$findBy = 'id';
+            $query = [];
+            foreach ($data as $key => $value) {
+				if (isset($key) && $value) {
+					$queryVal = $key . ' = "'.$value.'"';
+                    array_push($query, $queryVal);
+                }
+			}
+
+			$queryResult = implode(',', $query);			
+
+            if (!$queryResult)
+            {
+                return [
+                    'flag' => 1,
+                    'messages' => 'Gagal mengubah data'
+                ];
+            }
+            
+			$sql    =   "UPDATE {$this->tableName} SET ".$queryResult.", updated_at=now() WHERE ".$findBy."={$findByValue}";
+
+			$query  =   $this->db->query($sql);
+            $getError = $this->db->error();
+
+            if (!$getError['message']) {
+                return [
+                    'flag' => 0,
+                    'messages' => 'Berhasil mengubah data'
+                ];
+            } else {
+                return [
+                    'flag' => 1,
+                    'messages' => 'Gagal mengubah data'
+                ];
+            }
+        }
+
+        function deleteDataPermit ($field_name, $field_value) {
+            $sql    =   "DELETE FROM {$this->tableName} WHERE ".$field_name." = '".$field_value."'";
+            
+            $query  =   $this->db->query($sql);
+            $getError = $this->db->error();
+
+            if (!$getError['message']) {
+                return [
+                    'flag' => 0,
+                    'messages' => 'Berhasil menghapus data'
+                ];
+            } else {
+                return [
+                    'flag' => 1,
+                    'messages' => 'Gagal menghapus data'
+                ];
+            }
+        }
+    }
+?>
