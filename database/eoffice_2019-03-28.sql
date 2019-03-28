1c95487aa7f52d149da942ed81f6e511c6dd9518
Nurfirliana Muzanella <muzanella11@gmail.com>
2019-03-28 17:27:37 +0800

    [ADD] Backup database

--- database/eoffice_2019-03-28.sql
+++ database/eoffice_2019-03-28.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.7.24)
 # Database: eoffice
-# Generation Time: 2019-03-28 07:20:12 +0000
+# Generation Time: 2019-03-28 09:25:43 +0000
 # ************************************************************
 
 
@@ -18,6 +18,115 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table attendance
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `attendance`;
+
+CREATE TABLE `attendance` (
+  `id` int(100) unsigned NOT NULL AUTO_INCREMENT,
+  `signin_date` datetime DEFAULT NULL,
+  `signout_date` datetime DEFAULT NULL,
+  `status` int(20) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table division
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `division`;
+
+CREATE TABLE `division` (
+  `id` int(100) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table permit
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `permit`;
+
+CREATE TABLE `permit` (
+  `id` int(100) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT '',
+  `reason` varchar(100) DEFAULT NULL,
+  `start_date` datetime DEFAULT NULL,
+  `end_date` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table project
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `project`;
+
+CREATE TABLE `project` (
+  `id` int(100) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `assignee_to` varchar(100) DEFAULT NULL,
+  `deadline_date` datetime DEFAULT NULL,
+  `status` int(100) DEFAULT NULL,
+  `comment` int(100) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table project_comment
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `project_comment`;
+
+CREATE TABLE `project_comment` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `content` longtext,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table user
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(100) unsigned NOT NULL AUTO_INCREMENT,
+  `id_division` int(100) DEFAULT NULL,
+  `id_role` int(100) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  `avatar` varchar(50) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `join_date` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 
 # Dump of table user_role
